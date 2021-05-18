67d1cd95a20835eec68fab40b0c8d697e404a7c7
Bianca Elaine Tuno <biancatuno@gmail.com>
2021-05-18 13:39:00 +0800

    second commit

--- Project Calculator/Form1.cs
+++ Project Calculator/Form1.cs
@@ -17,5 +17,44 @@ namespace Project_Calculator
             InitializeComponent();
         }
 
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button16_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
